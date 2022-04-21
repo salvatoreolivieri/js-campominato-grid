2df3f4ee16419d044a4b5c0224747902d4a3b556
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-21 15:15:23 +0200

    Prima bozza square generator (ATTENZIONE: c'è il problema che me ne crea altri)

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ Con difficoltà 3 => tra 1 e 49
 
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+
+/* Step
+  1. Inseriamo una costante che al click del bottone "conferma" fa partire la nostra WebApp 
+  2. Funzione per generare i quadrati
+  3. Funzione che crea dei random number all'interno dei quadrati
+  4. Funzione che al click colora il quadrato di azzurro
+*/
+
+
+// 1. Inseriamo una costante che al click del bottone "conferma" fa partire la nostra WebApp
+
+const container = document.querySelector(".container");
+
+const btnConfirm = document.getElementById("conferma");
+btnConfirm.addEventListener("click", init);
+
+
+function init() {
+  for(let x = 1; x <= 100; x++){
+    const square = squareGenerator();
+  }
+}
+
+
+// 2. Funzione per generare i quadrati
+
+function squareGenerator() {
+  const square = document.createElement("div");
+  square.classList.add("square-100");
+  container.append(square);
+
+  return square;
+}
